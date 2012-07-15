abaeea8da1eb5c0bb279a14f9c70c32bfbaca79d
Juan <juan@juan-XPS-L412Z.(none)>
2012-07-14 23:36:28 -0300

    Implemented a safe flow for users trying to find non existence projects

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -2,4 +2,12 @@ require 'spec_helper'
 
 describe ProjectsController do
 
+  describe "#show" do
+
+    it "should display and error for a missing project" do
+      get :show, id: "not-existing"
+      response.should redirect_to(projects_path)
+      flash[:alert].should eql("The project you were looking for could not be found.")
+    end
+  end
 end
