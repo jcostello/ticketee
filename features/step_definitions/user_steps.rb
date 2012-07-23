990f8709a16cd394b8e6766885f3f145014faf4c
Juan <juan@juan-XPS-L412Z.(none)>
2012-07-23 07:45:01 -0300

    added feature for signin in via the Devise-provided form

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,6 +1,8 @@
 Given /^there are the following users:$/ do |table|
   table.hashes.each do |attributes|
+    confirmed = attributes.delete("confirmed") == "true"
     @user = User.create!(attributes)
+    @user.confirm! if confirmed
   end
 end
 
